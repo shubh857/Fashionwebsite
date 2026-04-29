79d63ff73215cbba117c86fd3f359f43b121e438
shubh857 <shubhampad123@gmail.com>
2026-04-29 12:25:13 +0530

    changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -55,10 +55,10 @@ const Login = () => {
         </div>
         <div className="container text-center mt-5 mb-5">
             <div className="row justify-content-center">
-                <div col-12 col-sm-10 col-md-8 col-lg-6>
+                <div className="col-12 col-md-6 col-lg-2" >
                     <form onSubmit={handleSubmit}>
-                        <input className="form-control contact3" name="email" value={loginData.email} onChange={handleChange} type="email" placeholder="Enter Email"></input><br /><br />
-                        <input className="form-control contact3" name="password" value={loginData.password} onChange={handleChange} type="password" placeholder="Enter Password"></input><br /><br />
+                        <input className="form-control contact3" name="email" value={loginData.email} onChange={handleChange} type="email" placeholder="Enter Email"></input><br />
+                        <input className="form-control contact3" name="password" value={loginData.password} onChange={handleChange} type="password" placeholder="Enter Password"></input><br />
                         <button className="btn btn-danger mb-3 logbutton">LOGIN</button>
                     </form>
                 </div>
